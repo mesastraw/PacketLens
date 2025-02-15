8d55a4ba41a27be800084b6640b0438313990ccb
Strawberrymilksh4kes <abdoalrzag@outlook.com>
2025-02-14 20:41:24 -0500

    Setup server

--- code/c/main.c
+++ code/c/main.c
@@ -7,22 +7,18 @@
 #include <pcap/pcap.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
+#include <sys/un.h>
 
 // This is my callback function passed to pcap_loop and is called each time a
 // packet is recieved and it will send packets to the go program
-void myCallback(u_char *args, const struct pcap_pkthdr *pkhdr, const u_char *packet) {
-}
+void myCallback(u_char *args, const struct pcap_pkthdr *pkhdr, const u_char *packet) {}
 
 // Packet Handling Functions
 void setupFilters(pcap_t **handle, struct bpf_program *fp, char filters[], bpf_u_int32 *net);
 pcap_t *setupDevice(bpf_u_int32 *net, bpf_u_int32 *mask);
 
-// Gonna need a for loop that continusly checks
-// the fiters sent from the go program
-// possibly get the go program to send a string to argv ?
-// use pipes?
-// Setup a way to dynamiclly update filters
 int main() {
   pcap_t *handler;               // Session handler
   char filters[] = "tcp or udp"; // This is our filters
@@ -30,14 +26,45 @@ int main() {
   bpf_u_int32 submask;           // mask of sniffer device
   bpf_u_int32 net;               // Ip of sniffer device
 
+  // Sockets and stuff
+  struct sockaddr_un serverAddr;      // Our server
+  struct sockaddr_storage clientAddr; // Our client who we are accepting a connection from
+
   int sck = socket(AF_UNIX, SOCK_STREAM, 0);
 
+  serverAddr.sun_family = AF_UNIX;                     // Says this is a unix socket
+  strcpy(serverAddr.sun_path, "/tmp/packetlens.sock"); // This sets our unix socket file
+  if (bind(sck, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
+    perror("Error binding to socket: ");
+    exit(EXIT_FAILURE);
+  }
+
+  // Listen for connections 5 max queus
+  if (listen(sck, 5) == -1) {
+    perror("Couldnt Get connection: ");
+    exit(EXIT_FAILURE);
+  }
+
+  // Accept connection from client
+  socklen_t clientLen = sizeof(clientAddr);
+  int clientSocket = accept(sck, (struct sockaddr *)&clientSocket, &clientLen);
+
+  // Write data to client
+  char *data = "Hello Go!!";
+  // Put send in callback function to send packts
+  send(clientSocket, data, strlen(data), 0);
+
+  // Close connection dont need it right now
+  shutdown(clientSocket, 2);
+
+  /*
   // This part of the program is all for getting packets
   setupDevice(&net, &submask);
   // Compiles and sets filters
   setupFilters(&handler, &fp, filters, &net);
   // Time to get sniffn
   pcap_loop(handler, -1, myCallback, NULL);
+  */
   return 0;
 }
 
@@ -68,16 +95,19 @@ pcap_t *setupDevice(bpf_u_int32 *net, bpf_u_int32 *subMask) {
   return handler;
 }
 
-void setupFilters(pcap_t **handler, struct bpf_program *fp, char filters[], bpf_u_int32 *net) {
+void setupFilters(pcap_t **handler, struct bpf_program *fp, char filters[],
+                  bpf_u_int32 *net) {
   // Compile filters
   if (pcap_compile(*handler, fp, filters, 0, *net) == PCAP_ERROR) {
-    fprintf(stderr, "Couldnt't parse filters %s: %s\n", filters, pcap_geterr(*handler));
+    fprintf(stderr, "Couldnt't parse filters %s: %s\n", filters,
+            pcap_geterr(*handler));
     exit(EXIT_FAILURE);
   }
 
   // Set our filters
   if (pcap_setfilter(*handler, fp) == PCAP_ERROR) {
-    fprintf(stderr, "Error Setting filter: %s: %s", filters, pcap_geterr(*handler));
+    fprintf(stderr, "Error Setting filter: %s: %s", filters,
+            pcap_geterr(*handler));
     exit(EXIT_FAILURE);
   }
 }
